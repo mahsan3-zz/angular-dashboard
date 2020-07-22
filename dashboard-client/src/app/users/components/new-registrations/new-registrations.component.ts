5eb92607451c15eaca2ce42e6b19e6a317268cd3
Muhammad Ahsan <mm.ahsan@hotmail.com>
2020-07-22 19:48:27 -0400

    Created input, some styling, and passing data from parent component to display

--- dashboard-client/src/app/users/components/new-registrations/new-registrations.component.ts
+++ dashboard-client/src/app/users/components/new-registrations/new-registrations.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-new-registrations',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-registrations.component.scss']
 })
 export class NewRegistrationsComponent implements OnInit {
+
+  @Input() newRegistrationsCount: number;
 
   constructor() { }
 
