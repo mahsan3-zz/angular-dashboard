6d563076c03981fb46d6e49c5f1daacf08fcb0b5
Muhammad Ahsan <mm.ahsan@hotmail.com>
2020-07-22 18:37:23 -0400

    Created selectors for user state

--- dashboard-client/src/app/users/store/index.ts
+++ dashboard-client/src/app/users/store/index.ts
@@ -1,7 +1,31 @@
 import {CoreState} from "../../core/store";
+import {createFeatureSelector, createSelector} from "@ngrx/store";
+import {UsersState} from "./users.reducers";
 
 export const SLICE_ID_USERS = 'users';
 
-export interface UsersState extends CoreState{
-  SLICE_ID_USERS: {}
+export interface UsersInfoState extends CoreState{
+  SLICE_ID_USERS: UsersState
 }
+
+const getUserState = createFeatureSelector<UsersState>(SLICE_ID_USERS);
+
+export const isLoading = createSelector(
+  getUserState,
+  state => state.isLoading
+);
+
+export const getRegistrationsToday = createSelector(
+  getUserState,
+  state => state.registrationsToday
+);
+
+export const getNewContentUploaded = createSelector(
+  getUserState,
+  state => state.newContentUploaded
+);
+
+export const getAverageSession = createSelector(
+  getUserState,
+  state => state.averageSession
+);
