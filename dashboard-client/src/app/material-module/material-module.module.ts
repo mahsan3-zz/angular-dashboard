6997489fc51f0f86661d1f5275fb6965a6c52ce0
Muhammad Ahsan <mm.ahsan@hotmail.com>
2020-07-22 11:15:09 -0400

    Created module just for material imports

---
+++ dashboard-client/src/app/material-module/material-module.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+
+
+@NgModule({
+  declarations: [],
+  imports: [],
+  exports: [
+    MatSidenavModule,
+    MatGridListModule,
+    MatListModule,
+    MatIconModule
+  ]
+})
+export class MaterialModuleModule { }
