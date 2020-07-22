2c6004e14f5d20abee06c9e0e1a4d2f55fd08195
Muhammad Ahsan <mm.ahsan@hotmail.com>
2020-07-22 12:33:38 -0400

    Dynamically generating grid

--- dashboard-client/src/app/users/containers/users-container/users-container.component.ts
+++ dashboard-client/src/app/users/containers/users-container/users-container.component.ts
@@ -1,13 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Tile {
+  cols: number;
+  rows: number;
+}
+
 @Component({
   selector: 'app-users-container',
   templateUrl: './users-container.component.html',
   styleUrls: ['./users-container.component.scss']
 })
 export class UsersContainerComponent implements OnInit {
+  tiles: Tile[];
 
-  constructor() { }
+  constructor() {
+    this.tiles = [
+      {cols: 3, rows: 1},
+      {cols: 1, rows: 1},
+      {cols: 2, rows: 1}
+    ];
+  }
 
   ngOnInit(): void {
   }
--- dashboard-client/src/app/users/users.module.ts
+++ dashboard-client/src/app/users/users.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersContainerComponent } from './containers/users-container/users-container.component';
 import {MaterialModule} from "../material-module/material.module";
+import {CommonModule} from "@angular/common";
 
 
 @NgModule({
   declarations: [UsersContainerComponent],
   imports: [
+    CommonModule,
     MaterialModule,
     UsersRoutingModule
   ]
