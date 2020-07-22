583ab9d848dfa5357dbb95805b9e988d185e44e0
Muhammad Ahsan <mm.ahsan@hotmail.com>
2020-07-22 11:52:11 -0400

    Created users container component

---
+++ dashboard-client/src/app/users/users-routing.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import {UsersContainerComponent} from "./containers/users-container/users-container.component";
+
+const routes: Routes = [{ path: '', component: UsersContainerComponent }];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class UsersRoutingModule { }
