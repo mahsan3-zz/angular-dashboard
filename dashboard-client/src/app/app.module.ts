362b95556248bf6fa083d2cee3e66cbeb1b11070
Muhammad Ahsan <mm.ahsan@hotmail.com>
2020-07-22 11:15:22 -0400

    Registering new modules

--- dashboard-client/src/app/app.module.ts
+++ dashboard-client/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MaterialModuleModule} from "./material-module/material-module.module";
+import { WelcomePageModule } from './welcome-page/welcome-page.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModuleModule,
+    WelcomePageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
