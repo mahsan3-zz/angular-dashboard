69d18c6b43778818655bb5d36e4036ba97dea737
Muhammad Ahsan <mm.ahsan@hotmail.com>
2020-07-22 19:59:18 -0400

    Created input, some styling, and passing data from parent component to display

--- dashboard-client/src/app/users/components/average-session/average-session.component.ts
+++ dashboard-client/src/app/users/components/average-session/average-session.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-average-session',
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./average-session.component.scss']
 })
 export class AverageSessionComponent implements OnInit {
+  @Input() averageSessions: string;
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
 }
